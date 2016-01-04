ff907e95a1f236634a7f4acf2bae6b4249ef9105
JaneWong1996 <jane_wong_1996@hotmail.com>
2016-01-04 15:29:10 +0800

    [Jane] try catch PublicLogin.aspx.cs

--- BusinessLogicLayer/UserBLL.cs
+++ BusinessLogicLayer/UserBLL.cs
@@ -116,10 +116,7 @@ namespace BusinessLogicLayer
             return returnMessage;
         }
 
-        public void hhh()
-        {
-
-        }
+        
 
         //public void InsertUser(string p1, string p2, string p3, string p4, string p5)
         //{
